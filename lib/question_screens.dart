b54e77a0bae597f5ad98395f2ad8a140be2193ad
Katie Andor <superkga5678@gmail.com>
2024-10-08 12:28:16 -0400

    created start screen and blank stateuful widgets

---
+++ lib/question_screens.dart
@@ -0,0 +1,2 @@
+//just return a text wdiget and be stateful
+//return material app and be stateful
---
+++ lib/start_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Opacity(
+            opacity: 0.5,
+            child: Image.asset(
+              'assets/assets/images/quiz-logo.png',
+              width: 300,
+              color: const Color.fromARGB(170, 255, 255, 255),
+            ),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            "Try your luck on our quiz",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            icon: const Icon(
+              Icons.arrow_circle_right_outlined,
+              size: 40,
+            ),
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {},
+            label: const Text(
+              "Start Quiz",
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
